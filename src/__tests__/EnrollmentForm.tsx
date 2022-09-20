8b3727d5f60b8bbd7c8855ce9ddf26a7bbe99ad2
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-09-19 21:37:16 -0300

    test form validation page 1

--- src/__tests__/EnrollmentForm.tsx
+++ src/__tests__/EnrollmentForm.tsx
@@ -1,12 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import EnrollmentForm from "../components/EnrollmentForm";
+import userEvent from "@testing-library/user-event";
 
-describe("The EnrollmentForm component", () => {
+describe("The EnrollmentForm component - page 1", () => {
   it("asks for the full name", () => {
     render(<EnrollmentForm />);
-    const fullNameTextBox = screen.getByRole('textbox', {
-      name: /nome completo/i
+    const fullNameTextBox = screen.getByRole("textbox", {
+      name: /nome completo/i,
     });
     expect(fullNameTextBox).toBeInTheDocument();
-  })
+  });
+
+  it("alerts empty full name, WhatsApp and CNH", async () => {
+    render(<EnrollmentForm />);
+    const messages = [
+      'O campo nome é obrigatório',
+      'Informe um número de celular',
+      'Informe o número da sua CNH',
+    ];
+    const nextBtn = screen.getByRole("button", {
+      name: /próximo/i,
+    });
+    expect(screen.queryAllByRole('alert').length).toBe(0);
+    await userEvent.click(nextBtn);
+    const alerts = screen.queryAllByRole('alert');
+    expect(alerts.length).toBe(3);
+    const alertsTexts = alerts.map((alert) => alert.innerHTML)
+    for (const message of messages) {
+      expect(alertsTexts).toContainEqual(message);
+    }
+  });
 });
