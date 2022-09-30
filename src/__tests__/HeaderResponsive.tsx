db4aa0f9e825857d6eb778809df3dd32dbba6a97
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-09-30 19:42:52 -0300

    fix lint from HeaderResponsive test

--- src/__tests__/HeaderResponsive.tsx
+++ src/__tests__/HeaderResponsive.tsx
@@ -1,26 +1,30 @@
-import { render, screen, within } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { HeaderResponsive } from '../components/HeaderResponsive';
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { HeaderResponsive } from "../components/HeaderResponsive";
 
-const getLinks = () => ({
-  sobre: screen.getByRole('link', {
+const getLinks = (): {
+  sobre: Element;
+  lrmc: Element;
+  inscrevase: Element;
+} => ({
+  sobre: screen.getByRole("link", {
     name: /sobre/i,
   }),
-  lrmc: screen.getByRole('link', {
+  lrmc: screen.getByRole("link", {
     name: /lrmc/i,
   }),
-  inscrevase: screen.getByRole('link', {
+  inscrevase: screen.getByRole("link", {
     name: /inscreva-se/i,
   }),
 });
 
-describe('The responsive header', () => {
+describe("The responsive header", () => {
   it('starts with "Inscreva-se" button active', () => {
     render(<HeaderResponsive />);
     const { sobre, lrmc, inscrevase } = getLinks();
     expect(sobre.classList).toEqual(lrmc.classList);
     expect(sobre.classList).not.toEqual(inscrevase.classList);
-    expect(document.URL).not.toMatch('/#sobre');
+    expect(document.URL).not.toMatch("/#sobre");
   });
 
   it('allows to set "sobre" button to active', async () => {
@@ -29,12 +33,12 @@ describe('The responsive header', () => {
     await userEvent.click(sobre);
     expect(inscrevase.classList).toEqual(lrmc.classList);
     expect(inscrevase.classList).not.toEqual(sobre.classList);
-    expect(document.URL).toMatch('/#sobre');
+    expect(document.URL).toMatch("/#sobre");
   });
 });
 
-describe('it uses dropdown for small screens', () => {
-  test('RTL renders the default links', () => {
+describe("it uses dropdown for small screens", () => {
+  test("RTL renders the default links", () => {
     render(<HeaderResponsive />);
     const { sobre, lrmc, inscrevase } = getLinks();
     expect(sobre).toBeInTheDocument();
@@ -42,17 +46,17 @@ describe('it uses dropdown for small screens', () => {
     expect(inscrevase).toBeInTheDocument();
   });
 
-  test('clicking the button, RTL also renders the dropdown', async () => {
+  test("clicking the button, RTL also renders the dropdown", async () => {
     render(<HeaderResponsive />);
-    const banner = screen.getByRole('banner');
-    const button = within(banner).getByRole('button');
+    const banner = screen.getByRole("banner");
+    const button = within(banner).getByRole("button");
     await userEvent.click(button);
-    for (const name of [/sobre/i, /lrmc/i, /inscreva-se/i]) {
+    [/sobre/i, /lrmc/i, /inscreva-se/i].forEach((name) => {
       expect(
-        screen.getAllByRole('link', {
+        screen.getAllByRole("link", {
           name,
-        }),
+        })
       ).toHaveLength(2);
-    }
+    });
   });
 });
